27d35eb691d8267781b376d63a9cb6b0422f4ffc
Nero <179806498+Ner046@users.noreply.github.com>
2026-06-10 09:51:44 +0300

    Weather-App V1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const API_KEY = '22039a984b3a11087b041c898fbe563d';
+
+
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
+const errorMessage = document.getElementById('error-message');
 
 
 const weatherCard = document.querySelector('[data-weather-card]');
@@ -8,30 +12,99 @@ const weatherTemp = document.querySelector('[data-weather-temp]');
 const weatherHumidity = document.querySelector('[data-weather-humidity]');
 const weatherPrecipitation = document.querySelector('[data-weather-precipitation]');
 const weatherWind = document.querySelector('[data-weather-wind]');
-const weatherImg = document.querySelector('[data-weather-img]')
+const weatherImg = document.querySelector('[data-weather-img]');
+const weatherC = document.querySelector('[data-unit-btn = C]');
+const weatherF = document.querySelector('[data-unit-btn = F]')
 
 
-searchForm.addEventListener('submit', (e)=>{
+
+const weatherImages = {
+    Clear: "images/weather-images/sunny.png",
+    Clouds: "images/weather-images/cloudy.png",
+    Rain: "images/weather-images/rainy.png",
+    Drizzle: "images/weather-images/rainy.png",
+    Thunderstorm: "images/weather-images/thunder.png",
+    Snow: "images/weather-images/snowy.png",
+    Mist: "images/weather-images/cloudy.png",
+    Fog: "images/weather-images/cloudy.png",
+    Haze: "images/weather-images/cloudy.png"
+};
+
+
+
+let celTemp;
+let ferTemp;
+
+
+searchForm.addEventListener('submit', async(e)=>{
     e.preventDefault();
 
-    const query = searchInput.value;
+    const query = searchInput.value.trim();
     if(!query) return;
 
-    const mockData = {
-        city:query,
-        temp:24,
-        conditionIcon: "images/weather-images/sunny.png",
-        humidity: "55%",
-        precipitation: "10%",
-        wind: "8mph"
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+
+    if(!response.ok){
+        errorMessage.classList.remove('hidden')
+        weatherCard.classList.add('hidden');
+        return;
 
     }
 
-    weatherCountry.textContent = mockData.city;
-    weatherTemp.textContent= mockData.temp;
-    weatherHumidity.textContent = mockData.humidity;
-    weatherPrecipitation.textContent = mockData.precipitation;
-    weatherWind.textContent = mockData.wind;
-    weatherImg.src = mockData.conditionIcon;
-    weatherCard.classList.remove('hidden')
+    const data = await response.json();
+    console.log(data);
+
+    errorMessage.classList.add('hidden');
+    
+    
+    celTemp = Math.round(data.list[0].main.temp-273.15);
+    ferTemp = Math.round((celTemp * 1.8) + 32);
+
+
+    const rainChance = Math.round(data.list[0].pop * 100);
+
+    const mainCondition = data.list[0].weather[0].main;
+
+    weatherImg.src = weatherImages[mainCondition] || "images/weather-images/cloudy.png";
+
+    weatherC.classList.add('active');
+    weatherF.classList.remove('active');
+    weatherC.classList.remove('opacity-50');
+    weatherF.classList.add('opacity-50');
+
+    
+    weatherCountry.textContent = data.city.name;
+    weatherTemp.textContent= `${celTemp}°C`;
+    weatherHumidity.textContent = `${data.list[0].main.humidity}%`;
+    weatherPrecipitation.textContent = `${rainChance}%`
+    weatherWind.textContent = `${data.list[0].wind.speed} m/s`;
+    weatherCard.classList.remove('hidden');
 })
+
+weatherC.addEventListener('click', (e)=>{
+    if (celTemp === undefined) return; 
+
+    weatherC.classList.add('active')
+    weatherF.classList.remove('active')
+
+    weatherC.classList.remove('opacity-50')
+    weatherF.classList.add('opacity-50')
+    weatherTemp.textContent= `${celTemp}°C`;
+    })
+
+     weatherF.addEventListener('click', (e)=>{
+        
+    if (ferTemp === undefined) return;
+
+    weatherF.classList.add('active')
+    weatherC.classList.remove('active')
+
+    weatherF.classList.remove('opacity-50')
+    weatherC.classList.add('opacity-50')
+
+    weatherTemp.textContent= `${ferTemp}°F`;
+
+    })
+
